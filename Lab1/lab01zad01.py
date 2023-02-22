bfb3b50755050793615e9293d3ba8240d47d90e4
awawron <a.wawron.693@studms.ug.edu.pl>
2023-02-22 14:55:01 +0100

    First task done, second task started

--- Lab1/lab01zad01.py
+++ Lab1/lab01zad01.py
@@ -1,2 +1,13 @@
 def prime(n):
-    pass
+    for i in range(2, int(n/2) + 1):
+        if (n % i) == 0:
+            return False
+
+    return True
+
+def select_primes(x):
+    re = []
+    for number in x:
+        if prime(number):
+            re.append(number)
+    return re
---
+++ Lab1/lab01zad02.py
@@ -0,0 +1,7 @@
+import pandas
+import matplotlib
+
+miasta = pandas.read_csv('miasta.csv')
+
+print(miasta)
+print('huh')
