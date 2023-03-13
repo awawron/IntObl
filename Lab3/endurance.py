213cba12b760aec84cf50575ab97f9cadb091015
awawron <a.wawron.693@studms.ug.edu.pl>
2023-03-13 20:50:21 +0100

    started lab3

---
+++ Lab3/endurance.py
@@ -0,0 +1,66 @@
+import pygad
+import numpy
+
+# definiujemy parametry chromosomu
+# geny to liczby: 0 lub 1
+gene_space = [0, 1]
+
+# definiujemy funkcję fitness
+
+
+def fitness_func(solution, solution_idx):
+    fitness = 0
+
+    return fitness
+
+
+fitness_function = fitness_func
+
+# ile chromsomów w populacji
+# ile genow ma chromosom
+sol_per_pop = 10
+num_genes = 6
+
+# ile wylaniamy rodzicow do "rozmanazania" (okolo 50% populacji)
+# ile pokolen
+# ilu rodzicow zachowac (kilka procent)
+num_parents_mating = 5
+num_generations = 50
+keep_parents = 2
+
+# jaki typ selekcji rodzicow?
+# sss = steady, rws=roulette, rank = rankingowa, tournament = turniejowa
+parent_selection_type = "sss"
+
+# w il =u punktach robic krzyzowanie?
+crossover_type = "single_point"
+
+# mutacja ma dzialac na ilu procent genow?
+# trzeba pamietac ile genow ma chromosom
+mutation_type = "random"
+mutation_percent_genes = 8
+
+# inicjacja algorytmu z powyzszymi parametrami wpisanymi w atrybuty
+ga_instance = pygad.GA(gene_space=gene_space,
+                       num_generations=num_generations,
+                       num_parents_mating=num_parents_mating,
+                       fitness_func=fitness_function,
+                       sol_per_pop=sol_per_pop,
+                       num_genes=num_genes,
+                       parent_selection_type=parent_selection_type,
+                       keep_parents=keep_parents,
+                       crossover_type=crossover_type,
+                       mutation_type=mutation_type,
+                       mutation_percent_genes=mutation_percent_genes)
+
+# uruchomienie algorytmu
+ga_instance.run()
+
+# podsumowanie: najlepsze znalezione rozwiazanie (chromosom+ocena)
+solution, solution_fitness, solution_idx = ga_instance.best_solution()
+print("Parameters of the best solution : {solution}".format(solution=solution))
+print("Fitness value of the best solution = {solution_fitness}".format(
+    solution_fitness=solution_fitness))
+
+# wyswietlenie wykresu: jak zmieniala sie ocena na przestrzeni pokolen
+ga_instance.plot_fitness()
